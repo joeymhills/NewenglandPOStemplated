c852e95091b06544e14159095401978847f3914a
Joey Hills <joeymhills@gmail.com>
2023-05-03 10:49:21 -0400

    fad

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,11 +111,15 @@
     var scroll = $(window).scrollTop();
     if (scroll >= 30) {
       $(".cs-sticky-header").addClass("cs-sticky-active");
-      $(".header-logo").attr('src','assets/img/logo.png')
+      /*
+      $(".header-logo").attr('src','assets/img/logo.png');
+      */
     } 
     else {
       $(".cs-sticky-header").removeClass("cs-sticky-active");
+      /*
       $(".header-logo").attr("src","assets/img/white on transparent.png");
+      */
       }
   }
 
